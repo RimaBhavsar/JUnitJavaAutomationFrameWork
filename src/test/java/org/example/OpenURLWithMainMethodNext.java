d9c90edcceb2512ca827d3ab36cf7a88794ad4dd
Rima Bhavsar <rvbhavsar@yahoo.com>
2022-05-03 14:58:45 +0100

    first commit

---
+++ src/test/java/org/example/OpenURLWithMainMethodNext.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenURLWithMainMethodNext {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.next.co.uk/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.close();
+
+
+    }
+}
---
+++ src/test/java/org/example/OpenURLWithMainMethodPoundland.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenURLWithMainMethodPoundland {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.poundland.co.uk/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.close();
+    }
+}
